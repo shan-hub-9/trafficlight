2b33feb0bf00a9cb50226d6fac28e5d0ee37e0d3
shahnawaz <mdshahnawazkhan513@gmail.com>
2024-12-06 14:11:46 +0530

    Add files via upload

---
+++ traffic/script.js
@@ -0,0 +1,21 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const sun = document.getElementById('sun');
+    
+    
+    sun.addEventListener('mouseover', function() {
+     
+      sun.style.transform = "scale(1.3)";
+      sun.style.boxShadow = "0 0 0px rgba(255, 255, 0, 1)"; 
+      sun.style.backgroundColor = "orange"; 
+    });
+  
+    sun.addEventListener('mouseout', function() {
+      sun.style.transform = "scale(1)";
+      sun.style.boxShadow = "0 0 0px rgba(255, 255, 0, 0.7)"; 
+      sun.style.backgroundColor = "yellow"; 
+    });
+    sun.style.transform = "scale(1)";
+    sun.style.boxShadow = " 0 0 0px rgb(3, 144, 50)"
+    sun.style.backgroundColor = "green";
+  });
+  
